dc4bc2466a3bb6f52cb23d28b88156a8dad8cb06
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-02-19 21:07:23 +0100

    version 0.1.10

--- src/cli/app.rs
+++ src/cli/app.rs
@@ -8,7 +8,7 @@ use clap::{App, Arg};
 
 pub fn run() {
     let matches = App::new("Command Example")
-        .version("0.1.8")
+        .version("0.1.10")
         .author("Blas Rodriguez Irizar. <rodrigblas@gmail.com>")
         .about("Search for command examples directly on your command line")
         .arg(
