b6e8b4a0631d00f78612797fe7715f4eca0c2b37
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-04-19 23:29:05 +0200

    Version 0.2.6

--- src/cli/app.rs
+++ src/cli/app.rs
@@ -11,7 +11,7 @@ pub fn run() {
     let db = CommandsDB::new();
 
     let matches = App::new("Command Example")
-        .version("0.2.5")
+        .version("0.2.6")
         .setting(AppSettings::AllowMissingPositional)
         .author("Blas Rodriguez Irizar. <rodrigblas@gmail.com>")
         .about("Search for command examples directly on your command line")
